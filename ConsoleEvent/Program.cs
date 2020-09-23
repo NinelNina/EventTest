bd68a1aab9b1f0cc45e7567a1189088d125ef477
Nina <ninkr@list.ru>
2020-09-23 09:05:28 +0300

    Test

---
+++ ConsoleEvent/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loginSource = "user";
+            string passwordSource = "123";
+
+            Write("Введите логин: ");
+            string login = ReadLine();            
+            
+            Write("Введите пароль: ");
+            string password = ReadLine();
+
+            if (login == loginSource && password == passwordSource)
+            {
+                WriteLine("Вход разрешён");
+            }
+            else
+            {
+                WriteLine("Вход запрещён");
+            }
+        }
+
+    }
+}
